20fc569b16e16fad896fa1e7ec12a6f6629fd504
Juan-P1224 <juanp.cardonag@uqvirtual.edu.co>
2022-09-25 21:04:36 -0500

    Primer comit <3

---
+++ negocio/src/main/java/co/edu/uniquindio/unicine/NegocioApplication.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.unicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class NegocioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NegocioApplication.class, args);
+    }
+}
